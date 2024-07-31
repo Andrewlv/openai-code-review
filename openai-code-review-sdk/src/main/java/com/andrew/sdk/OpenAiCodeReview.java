e7aa15c06551ea201be65dbde38614930cf0ed9e
Andrewlv <2289380832@qq.com>
2024-07-31 19:35:29 +0800

    feat: 代码检出

--- openai-code-review-sdk/src/main/java/com/andrew/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/andrew/sdk/OpenAiCodeReview.java
@@ -1,7 +1,29 @@
 package com.andrew.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+
+        // 1.代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder sb = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("exit with code: " + exitCode);
+        System.out.println("评审代码：" + sb);
     }
 }
